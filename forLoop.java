572c439840f566c3959b76d457ab75171ece4655
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-09-15 01:05:55 +0530

    pq

--- forLoop.java
+++ forLoop.java
@@ -24,11 +24,58 @@ public class forLoop {
 
 
         //pq 4 print number of table input by user
-        System.out.print("Enter number : ");
-        int n = sc.nextInt();
-        for(int i=1; i<=10; i++){
-            System.out.println(n*i);
+        // System.out.print("Enter number : ");
+        // int n = sc.nextInt();
+        // for(int i=1; i<=10; i++){
+        //     System.out.println(n*i);
+        // }
+
+
+        //pq5 print even numbers till n
+        // System.out.print("Enter n : ");
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n; i++){
+        //     if(i%2 == 0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+
+        //pq 6  Make a menu driven program. The user can enter 2 numbers, either 1 or 0. 
+// If the user enters 1 then keep taking input from the user for a student’s marks(out of 100). 
+// If they enter 0 then stop.
+// If he/ she scores :
+// Marks >=90 -> print “This is Good”
+// 89 >= Marks >= 60 -> print “This is also Good”
+// 59 >= Marks >= 0 -> print “This is Good as well”
+	 
+int choice;
+do {
+    System.out.println("Enter 1 to input marks or 0 to stop: ");
+    choice = sc.nextInt();
+    
+    if (choice == 1) {
+        System.out.println("Enter student's marks (out of 100): ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("This is Good");
+        } else if (marks >= 60 && marks <= 89) {
+            System.out.println("This is also Good");
+        } else if (marks >= 0 && marks <= 59) {
+            System.out.println("This is Good as well, because marks don’t matter but our effort does.");
+        } else {
+            System.out.println("Invalid marks! Please enter a value between 0 and 100.");
         }
+    } else if (choice != 0) {
+        System.out.println("Invalid choice! Please enter 1 or 0.");
+    }
+} while (choice != 0);
+
+System.out.println("Program terminated.");
 
     }
+
+
+    
 }
