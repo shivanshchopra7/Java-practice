ded8dce2ca870b87de638a9858b8d76ec175a421
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-09-15 21:32:33 +0530

    Structuring

--- Loops/doWhile.java
+++ Loops/doWhile.java
@@ -1,3 +1,4 @@
+package Loops;
 public class doWhile {
     public static void main(String args[]) {
 
--- Loops/whileLoop.java
+++ Loops/whileLoop.java
@@ -1,3 +1,4 @@
+package Loops;
 public class whileLoop {
     public static void main(String args[]){
 
